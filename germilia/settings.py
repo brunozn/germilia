c45900ee243f1e328efae156d409fff077ceb056
brunozn <brunojndias@gmail.com>
2022-08-27 23:37:34 -0300

    ajuste de datas

--- germilia/settings.py
+++ germilia/settings.py
@@ -116,8 +116,8 @@ USE_L10N = False
 # DATE_FORMAT = "d/m/Y"
 DATE_FORMAT = ( ( 'd/m/Y' ))
 DATE_INPUT_FORMATS = ( ('%d/%m/%Y'),)
-DATETIME_FORMAT = (( 'd-m-Y H:i' ))
-DATETIME_INPUT_FORMATS = (('%d-%m-%Y %H:%i'),)
+DATETIME_FORMAT = (( 'd/m/Y H:i' ))
+DATETIME_INPUT_FORMATS = (('%d/%m/%Y %H:%i'),)
 
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
