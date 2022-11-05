32f9a9f776a4907b2b5c3707445f0a53f2eb5607
brunozn <brunojndias@gmail.com>
2022-11-05 02:40:49 -0300

    scripts para otimizar restart do projeto

---
+++ scripts/listBank.sql
@@ -0,0 +1,9 @@
+INSERT INTO gerenciamento_banco(cod, nome, "fullName")
+VALUES (1, 'BANCO DO BRASIL', 'Banco do Brasil S.A'),
+       (260, 'NU PAGAMENTOS', 'NU PAGAMENTOS S.A.'),
+       (77, 'BANCO INTER', 'Banco Inter S.A.'),
+       (380, 'PICPAY', 'PICPAY INSTITUIÇÃO DE PAGAMENTO S.A.'),
+       (237, 'BCO BRADESCO', 'Banco Bradesco S.A.'),
+       (336,'C6', 'BCO C6 S.A.'),
+       (104,'CAIXA', 'CAIXA ECONOMICA FEDERAL'),
+       (33,'SANTANDER', 'BANCO SANTANDER (BRASIL) S.A.')
