b8916b86b6698e718711b6dcff377c5e4f351763
brunozn <brunojndias@gmail.com>
2022-11-15 18:23:20 -0300

    Historico de pagamentos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from dashboard import views
 
-
 urlpatterns = [
     path('contratos/', views.contrat_list, name='contratos_list'),
+    path('pagamentos/', views.historic_pay, name='historic_pay'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from gerenciamento.models import PlanContract
+from gerenciamento.models import PlanContract, PlanContractPayment
 
 
 # Create your views here.
@@ -8,3 +8,9 @@ def contrat_list(request):
     membro = request.user.membro
     contratos = PlanContract.objects.filter(membro_id=membro).order_by('-date_vencimento')
     return render(request, 'contrato_list.html', {'contratos': contratos})
+
+
+def historic_pay(request):
+    membro = request.user.membro
+    pays = PlanContractPayment.objects.filter(contrato_plano__membro=membro)
+    return render(request, 'historic_pays.html', {'pays': pays})
