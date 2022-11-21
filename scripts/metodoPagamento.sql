0af59b41af79dc9f97e497ff4dccad02a4eee10f
brunozn <brunojndias@gmail.com>
2022-11-21 01:19:04 -0300

    Criando script de metodoPagamento e de pagamentos e iniciando com as infos de meme

---
+++ scripts/metodoPagamento.sql
@@ -0,0 +1,9 @@
+insert into gerenciamento_metodopagamento(nome, banco_id)
+    VALUES
+        ('PIX BRASIL', 1),
+        ('TED BRASIL', 1),
+        ('DOC BRASIL', 1),
+        ('TED CAIXA', 7),
+        ('PIX INTER', 3),
+        ('PIX NU', 2),
+        ('Em mãos', 9);
