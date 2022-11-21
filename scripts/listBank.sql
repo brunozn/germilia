9353bccdc1d01aba3ffa3aeaac152cabd73c8b16
brunozn <brunojndias@gmail.com>
2022-11-21 01:17:52 -0300

    Adicionando a opção pagamento em especie no script de listBank

--- scripts/listBank.sql
+++ scripts/listBank.sql
@@ -1,12 +1,14 @@
 INSERT INTO gerenciamento_banco(cod_banco, nome_banco, nome_completo_banco)
-VALUES (1, 'BANCO DO BRASIL', 'Banco do Brasil S.A'),
-       (260, 'NU PAGAMENTOS', 'NU PAGAMENTOS S.A.'),
-       (77, 'BANCO INTER', 'Banco Inter S.A.'),
-       (380, 'PICPAY', 'PICPAY INSTITUIÇÃO DE PAGAMENTO S.A.'),
-       (237, 'BCO BRADESCO', 'Banco Bradesco S.A.'),
-       (336,'C6', 'BCO C6 S.A.'),
-       (104,'CAIXA', 'CAIXA ECONOMICA FEDERAL'),
-       (33,'SANTANDER', 'BANCO SANTANDER (BRASIL) S.A.')
+VALUES
+    (1, 'BANCO DO BRASIL', 'Banco do Brasil S.A'),
+    (260, 'NU PAGAMENTOS', 'NU PAGAMENTOS S.A.'),
+    (77, 'BANCO INTER', 'Banco Inter S.A.'),
+    (380, 'PICPAY', 'PICPAY INSTITUIÇÃO DE PAGAMENTO S.A.'),
+    (237, 'BCO BRADESCO', 'Banco Bradesco S.A.'),
+    (336,'C6', 'BCO C6 S.A.'),
+    (104,'CAIXA', 'CAIXA ECONOMICA FEDERAL'),
+    (33,'SANTANDER', 'BANCO SANTANDER (BRASIL) S.A.'),
+    (0, 'ESPECIE', 'Dinheiro em mãos');
 
 
 
